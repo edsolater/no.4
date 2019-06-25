53d653b69e18a08e9c83a7ccca7bdb802843b23f
edsol <edsolater@qq.com>
2019-06-25 20:55:23 +0800

    展示网格部署完成

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from 'react'
-import { VerticalNavigator } from './components'
+import * as componentItems from './components'
+
+const components = Object.values(componentItems)
 
 export default function App() {
-  return <VerticalNavigator />
+  return (
+    <div
+      className="grid"
+      style={{
+        display: 'grid',
+        gridAutoFlow: 'column',
+        gridGap: 10,
+        justifyItems: 'center',
+        alignItems: 'center'
+      }}
+    >
+      {components.map((Component, idx) => (
+        <div key={idx}>
+          <div
+            style={{
+              background: 'hsla(0, 0%, 40%, 0.2)',
+              width: 'max-content'
+            }}
+          >
+            <Component />
+          </div>
+          <p>{Component.name}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
--- src/components/VerticalNavigator.js
+++ src/components/VerticalNavigator.js
@@ -4,16 +4,16 @@ export default function VerticalNavigator({
   items = [
     {
       // r: 20,
-      onClick(item, index) {
+      onClick(item, idx) {
         console.log('item.r: ', item.r)
-        return console.log(`click big circle ${index}`)
+        return console.log(`click big circle ${idx}`)
       }
     },
     {
       // r: 24,
-      onClick(item, index) {
+      onClick(item, idx) {
         console.log('item.r: ', item.r)
-        return console.log(`click big circle ${index}`)
+        return console.log(`click big circle ${idx}`)
       }
     },
     {},
@@ -21,48 +21,49 @@ export default function VerticalNavigator({
     {},
     {}
   ],
-  activeItemIndex = 1,
-  gutter,
+  activeItemIdx = 1,
+  gap,
 
   // 泛化性 Props
   width = 32,
   height,
-  fill = 'hsla(0, 0%, 70%,0.2)',
+  fill = 'hsla(0, 0%, 70%, 0.2)',
   fill_hover = 'hsl(25, 98%, 70%)',
   fill_active = 'hsl(238, 98%, 70%)',
   element // 手动指定渲染组件(传递已激发的组件)
 }) {
   return (
     <div style={{ width, height }}>
-      {items.map((item, index) => (
+      {items.map((item, idx) => (
         <svg
+          key={idx}
           viewBox="0 0 64 64"
           onClick={
-            (item.onClick && (() => item.onClick(item, index))) ||
-            (() => console.log(`click Indicator ${index}`))
+            (item.onClick && (() => item.onClick(item, idx))) ||
+            (() => console.log(`click Indicator ${idx}`))
           }
           style={{
             width: '100%',
             height: '100%',
-            marginBottom: index !== items.length - 1 && gutter
+            marginBottom: idx !== items.length - 1 && gap
           }}
         >
           <style>
             {`
-                  #indicator {
-                    transition: 400ms all ease
-                  }
-                  #indicator:hover {
-                    fill: ${fill_hover};
-                  }
-                  #indicator.active {
-                    fill: ${fill_active};
-                  }
+              #indicator {
+                transition: 400ms all ease
+              }
+              #indicator:hover {
+                fill: ${fill_hover};
+              }
+              #indicator.active {
+                fill: ${fill_active};
+              }
               `}
           </style>
           <g
             id="indicator"
-            class={index === activeItemIndex && 'active'}
+            className={idx === activeItemIdx ? 'active' : ''}
             fill={fill}
           >
             {element || <circle cx="32" cy="32" r={`${item.r || 16}`} />}
