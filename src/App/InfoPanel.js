4fcd1f58190aa9e9a5010126105a56ac4113bf16
edsolater <edsolater@qq.com>
2019-07-08 09:16:07 +0800

    增强table逻辑

--- src/App/InfoPanel.js
+++ src/App/InfoPanel.js
@@ -1,54 +1,47 @@
 import React from 'react'
-import { Table, Icon } from 'antd/es'
+import { Table, Card } from 'antd/es'
 
-const API = ({ tableData }) => {
+// react-component
+const APITable = ({ tableName, tableData }) => {
+  const createTable = ({ title = 'unknown', data }) => (
+    <Table
+      title={() => <p>{title}</p>}
+      columns={[
+        { title: 'Property', dataIndex: 'property' },
+        { title: '说明', dataIndex: 'description' },
+        { title: '值类型', dataIndex: 'type' },
+        { title: '默认值', dataIndex: 'default' }
+      ]}
+      dataSource={data}
+      pagination={false}
+    />
+  )
   if (Array.isArray(tableData)) {
     // 只需显示一个表单，传入的 tableData 就是表单项的数据
-    return (
-      <Table
-        title={() => <p>main</p>}
-        columns={[
-          { title: 'Property', dataIndex: 'property' },
-          { title: '说明', dataIndex: 'description' },
-          { title: '值类型', dataIndex: 'type' },
-          { title: '默认值', dataIndex: 'default' }
-        ]}
-        dataSource={tableData}
-        pagination={false}
-      />
-    )
+    return createTable({ title: tableName, data: tableData })
   } else {
     // 需要显示多个表单，传入的 tableData 需要拆分
-    return Object.entries(tableData).map(([tableName, tableData]) => (
-      <Table
-        title={() => <p>{tableName}</p>}
-        columns={[
-          { title: 'Property', dataIndex: 'property' },
-          { title: '说明', dataIndex: 'description' },
-          { title: '值类型', dataIndex: 'type' },
-          { title: '默认值', dataIndex: 'default' }
-        ]}
-        dataSource={tableData}
-        pagination={false}
-      />
-    ))
+    return Object.entries(tableData).map(([tableName, tableData]) =>
+      createTable({ title: tableName, data: tableData })
+    )
   }
 }
 
+// react component
 const InfoPanel = ({ selectedItemInfo }) => {
-  const { api } = selectedItemInfo
+  const { name: tableName, api } = selectedItemInfo
   return (
     <div
       style={{
         width: '100%',
         height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        background: '#6e6e6e'
+        background: '#6e6e6e',
+        padding:10
       }}
     >
-      <API tableData={api.tableData} />
+      <Card title="API">
+        <APITable tableName={tableName} tableData={api.tableData} />
+      </Card>
     </div>
   )
 }
